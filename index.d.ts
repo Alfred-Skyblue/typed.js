8359a2312bb7959e69dfc7575fd6dc6be18536d7
丶远方 <yangpanteng@gmail.com>
2023-03-17 12:26:16 +0800

    feat(index.d.ts): complete type declaration

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,175 @@
+interface TypedOptions {
+    /**
+     * @property {array} strings strings to be typed
+     */
+    strings?: string[],
+    /**
+     * @property {string} stringsElement ID of element containing string children
+     */
+    stringsElement?: string,
+
+    /**
+     * @property {number} typeSpeed type speed in milliseconds
+     */
+    typeSpeed?: number,
+
+    /**
+     * @property {number} startDelay time before typing starts in milliseconds
+     */
+    startDelay?: number,
+
+    /**
+     * @property {number} backSpeed backspacing speed in milliseconds
+     */
+    backSpeed?: number,
+
+    /**
+     * @property {boolean} smartBackspace only backspace what doesn't match the previous string
+     */
+    smartBackspace?: boolean,
+
+    /**
+     * @property {boolean} shuffle shuffle the strings
+     */
+    shuffle?: boolean,
+
+    /**
+     * @property {number} backDelay time before backspacing in milliseconds
+     */
+    backDelay?: number,
+
+    /**
+     * @property {boolean} fadeOut Fade out instead of backspace
+     */
+    fadeOut?: boolean,
+    /**
+     * @property {string} fadeOutClass css class for fade animation
+     */
+    fadeOutClass?: string,
+    /**
+     * @property {number} fadeOutDelay Fade out delay in milliseconds
+     */
+    fadeOutDelay?: number,
+
+    /**
+     * @property {boolean} loop loop strings
+     */
+    loop?: boolean,
+    /**
+     * @property {number} loopCount amount of loops
+     */
+    loopCount?: number,
+
+    /**
+     * @property {boolean} showCursor show cursor
+     */
+    showCursor?: boolean,
+    /**
+     * @property {string} cursorChar character for cursor
+     */
+    cursorChar?: string,
+    /**
+     * @property {boolean} autoInsertCss insert CSS for cursor and fadeOut into HTML <head>
+     */
+    autoInsertCss?: boolean,
+
+    /**
+     * @property {string} attr attribute for typing
+     * Ex: input placeholder, value, or just HTML text
+     */
+    attr?: string,
+
+    /**
+     * @property {boolean} bindInputFocusEvents bind to focus and blur if el is text input
+     */
+    bindInputFocusEvents?: boolean,
+
+    /**
+     * @property {string} contentType 'html' or 'null' for plaintext
+     */
+    contentType?: string,
+
+    /**
+     * Before it begins typing
+     * @param {Typed} self
+     */
+    onBegin?: (self:Typed) => void,
+
+    /**
+     * All typing is complete
+     * @param {Typed} self
+     */
+    onComplete?: (self:Typed) => void,
+
+    /**
+     * Before each string is typed
+     * @param {number} arrayPos
+     * @param {Typed} self
+     */
+    preStringTyped?: (arrayPos:number, self:Typed) => void,
+
+    /**
+     * After each string is typed
+     * @param {number} arrayPos
+     * @param {Typed} self
+     */
+    onStringTyped?: (arrayPos:number, self:Typed) => void,
+
+    /**
+     * During looping, after last string is typed
+     * @param {Typed} self
+     */
+    onLastStringBackspaced?: (self:Typed) => void,
+
+    /**
+     * Typing has been stopped
+     * @param {number} arrayPos
+     * @param {Typed} self
+     */
+    onTypingPaused?: (arrayPos:number, self:Typed) => void,
+
+    /**
+     * Typing has been started after being stopped
+     * @param {number} arrayPos
+     * @param {Typed} self
+     */
+    onTypingResumed?: (arrayPos:number, self:Typed) => void,
+
+    /**
+     * After reset
+     * @param {Typed} self
+     */
+    onReset?: (self:Typed) => void,
+
+    /**
+     * After stop
+     * @param {number} arrayPos
+     * @param {Typed} self
+     */
+    onStop?: (arrayPos:number, self:Typed) => void,
+
+    /**
+     * After start
+     * @param {number} arrayPos
+     * @param {Typed} self
+     */
+    onStart?: (arrayPos:number, self:Typed) => void,
+
+    /**
+     * After destroy
+     * @param {Typed} self
+     */
+    onDestroy?: (self:Typed) => void,
+}
+
 /**
  * Welcome to Typed.js!
- * @param {string} elementId HTML element ID _OR_ HTML element
- * @param {object} options options object
+ * @param {HTMLElement | string} elementId HTML element ID _OR_ HTML element
+ * @param {TypedOptions} options options object
  * @returns {object} a new Typed object
  */
-export default class Typed {
-    constructor(elementId: any, options: any);
+declare class Typed {
+    constructor(elementId: HTMLElement | string, options: TypedOptions);
     /**
      * Toggle start() and stop() of the Typed instance
      * @public
